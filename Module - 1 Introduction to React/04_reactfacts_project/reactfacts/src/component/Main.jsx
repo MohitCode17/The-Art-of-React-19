ba5d6a20cb811d9208727b3400f7d64866c5e3be
MohitCode17 <mohit.codes17@gmail.com>
2024-11-26 23:09:43 +0530

    complete first project - reactfacts

--- Module - 1 Introduction to React/04_reactfacts_project/reactfacts/src/component/Main.jsx
+++ Module - 1 Introduction to React/04_reactfacts_project/reactfacts/src/component/Main.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Main = () => {
-  return <h1>Main content goes here</h1>;
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul className="fact-lists">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 200K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
 };
 
 export default Main;
