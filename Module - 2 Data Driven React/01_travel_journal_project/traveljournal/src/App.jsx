4fd44c35953333de8497e0ad0eb687bfc6c7e2d6
MohitCode17 <mohit.codes17@gmail.com>
2024-11-27 20:20:37 +0530

    add entry component to traveljournal

--- Module - 2 Data Driven React/01_travel_journal_project/traveljournal/src/App.jsx
+++ Module - 2 Data Driven React/01_travel_journal_project/traveljournal/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
 import "./App.css";
+import Entry from "./components/Entry";
 
 const App = () => {
   return (
     <>
       <Header />
+      <main className="container">
+        <Entry />
+        <Entry />
+        <Entry />
+      </main>
     </>
   );
 };
