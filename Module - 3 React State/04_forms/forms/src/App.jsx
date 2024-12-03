0cf86fe52cd945e18ef3637afd59b389c95675eb
MohitCode17 <mohit.codes17@gmail.com>
2024-12-03 23:04:27 +0530

    react form - select box handle

--- Module - 3 React State/04_forms/forms/src/App.jsx
+++ Module - 3 React State/04_forms/forms/src/App.jsx
@@ -7,7 +7,8 @@ const App = () => {
     const password = formData.get("password");
     const employmentStatus = formData.get("employmentStatus");
     const dietaryRestrictions = formData.getAll("dietaryRestrictions");
-    console.log(dietaryRestrictions);
+    const favColor = formData.get("favColor");
+    console.log(favColor);
   };
 
   return (
@@ -73,6 +74,20 @@ const App = () => {
             Gluten-free
           </label>
         </fieldset>
+
+        <label htmlFor="favColor">What is your favorite color?</label>
+        <select id="favColor" name="favColor" defaultValue="" required>
+          <option value="" disabled>
+            -- Choose a color --
+          </option>
+          <option value="red">Red</option>
+          <option value="orange">Orange</option>
+          <option value="yellow">Yellow</option>
+          <option value="green">Green</option>
+          <option value="blue">Blue</option>
+          <option value="indigo">Indigo</option>
+          <option value="violet">Violet</option>
+        </select>
 
         <button>Submit</button>
       </form>
