76c1a3d9c0af86cd886760aa6c9f16119fd26e1c
MohitCode17 <mohit.codes17@gmail.com>
2024-12-07 17:47:06 +0530

    hugging face api setting up

--- Module - 3 React State/01_chef_claude/chefclaude/src/App.jsx
+++ Module - 3 React State/01_chef_claude/chefclaude/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Main from "./components/Main";
 
 const App = () => {
+  // console.log(import.meta.env.VITE_HF_API_KEY);
   return (
     <>
       <Header />
